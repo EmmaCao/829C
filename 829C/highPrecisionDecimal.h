7bff8627530456c8c1d541a4d06ae186beab2fc7
Emma <calyn2008@126.com>
2019-05-21 11:36:34 +0800

    高精度小数

---
+++ 829C/highPrecisionDecimal.h
@@ -0,0 +1,16 @@
+//
+//  highPrecisionDecimal.h
+//  829C
+//
+//  Created by Emma on 2019/5/21.
+//  Copyright © 2019 Emma. All rights reserved.
+//
+
+#ifndef highPrecisionDecimal_h
+#define highPrecisionDecimal_h
+
+#include <stdio.h>
+//高精度小数
+void highPrecisionDecimal(void);
+
+#endif /* highPrecisionDecimal_h */
---
+++ 829C/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  829C
+//
+//  Created by Emma on 2019/5/21.
+//  Copyright © 2019 Emma. All rights reserved.
+//
+
+#include <stdio.h>
+#include "highPrecisionDecimal.h"
+
+int main(int argc, const char * argv[]) {
+    
+    highPrecisionDecimal();
+    
+    return 0;
+}
